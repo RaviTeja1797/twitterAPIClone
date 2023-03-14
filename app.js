b4e05f34152e93ac853246c676a8f15d835734e9
Ravi Teja <ravi_teja_1797@yahoo.com>
2023-03-14 10:00:42 +0000

    Defined getNamesOfFollowingUsers - API-4 | getNamesOfMyFollowers - API-5 | getTweetDetails - API-6 | getTweetLikes - API-7 | getTweetReplies - API-8 | All are working as intended

--- app.js
+++ app.js
@@ -168,3 +168,140 @@ expressAppInstance.get('/user/tweets/feed/', authenticateUser, async(request, re
     console.log(`Error message ${e}`)
   }
 })
+
+
+// getNamesOfFollowingUsersAPI | API-4 | GET
+expressAppInstance.get('/user/following/', authenticateUser, async(request, response)=>{
+    let {username} = request;
+    const getNamesOfFollowingUsersQuery = `SELECT user.name FROM user WHERE user_id IN (
+        SELECT follower.following_user_id FROM follower INNER JOIN user ON follower.follower_user_id = user.user_id WHERE user.username like "${username}"
+    )`
+    try{
+        let arrayOfNameObjects = await databaseConnectionObject.all(getNamesOfFollowingUsersQuery);
+        response.send(arrayOfNameObjects)
+    }catch(e){
+        console.log(`Database Error ${e.message}`)
+    }
+})
+
+
+//getNamesOfMyFollowers | API-5 | GET 
+
+expressAppInstance.get('/user/followers/', authenticateUser, async(request, response)=>{
+    let {username} = request;
+    const getNamesOfFollowingUsersQuery = `SELECT user.name FROM
+    user INNER JOIN follower ON user.user_id = follower.follower_user_id
+    WHERE follower.following_user_id =(SELECT user_id FROM user WHERE user.username="${username}")`
+    try{
+        let arrayOfNameObjects = await databaseConnectionObject.all(getNamesOfFollowingUsersQuery);
+        response.send(arrayOfNameObjects)
+    }catch(e){
+        console.log(`Database Error ${e.message}`)
+    }
+})
+
+//getTweetDetailsAPI | API-6 | GET
+expressAppInstance.get("/tweets/:tweetId/", authenticateUser, async(request, response)=>{
+    let{tweetId} = request.params;
+    tweetId = parseInt(tweetId)
+    const{username} = request;
+    
+    let followingUsersWithTheirTweetIdsQuery = `SELECT follower.following_user_id, tweet.tweet_id FROM
+    follower INNER JOIN tweet ON follower.following_user_id = tweet.user_id WHERE follower.follower_user_id = (SELECT user_id FROM user WHERE username LIKE "${username}");`
+    try{
+        let arrayOfFollowingUsersWithTweetIds = await databaseConnectionObject.all(followingUsersWithTheirTweetIdsQuery)
+        console.log(arrayOfFollowingUsersWithTweetIds)
+        let possibleTweetIdsArray=[]
+        arrayOfFollowingUsersWithTweetIds.forEach((each)=>{
+            possibleTweetIdsArray.push(each["tweet_id"])
+        })
+        console.log(possibleTweetIdsArray.includes(tweetId))
+        if (possibleTweetIdsArray.includes(tweetId)){
+            //send the tweet details
+            //response.send('Its under development you would soon get the tweet details')
+            //we need to combine the tweet, like, reply tables and perform aggrigation
+            const getTweetDetailsQuery = `SELECT tweet, count(DISTINCT like_id) AS likes, count(DISTINCT reply_id) AS replies, tweet.date_time AS dateTime FROM
+            (tweet LEFT JOIN like ON tweet.tweet_id = like.tweet_id)AS likeTweet
+            LEFT JOIN reply ON tweet.tweet_id = reply.tweet_id
+            WHERE tweet.tweet_id = ${tweetId};`
+            try{
+                let tweetDetails = await databaseConnectionObject.get(getTweetDetailsQuery)
+                response.send(tweetDetails)
+            }catch(e){
+                console.log(`Database Error ${e.message}`)
+            }
+        }else{
+            response.status(401)
+            response.send('Invalid request')
+        }
+
+    }catch(e){
+        console.log(`Error message ${e.message}`)
+    }
+})
+
+//getTweetLikesAPI | API-7 | GET
+expressAppInstance.get("/tweets/:tweetId/likes", authenticateUser, async(request, response)=>{
+    let {tweetId} = request.params;
+    tweetId = parseInt(tweetId)
+    const{username} = request;
+
+    let followingUsersWithTheirTweetIdsQuery = `SELECT follower.following_user_id, tweet.tweet_id FROM
+    follower INNER JOIN tweet ON follower.following_user_id = tweet.user_id WHERE follower.follower_user_id = (SELECT user_id FROM user WHERE username LIKE "${username}");`
+
+    let arrayOfFollowingUsersWithTweetIds = await databaseConnectionObject.all(followingUsersWithTheirTweetIdsQuery)
+    console.log(arrayOfFollowingUsersWithTweetIds)
+    let possibleTweetIdsArray=[]
+    arrayOfFollowingUsersWithTweetIds.forEach((each)=>{
+        possibleTweetIdsArray.push(each["tweet_id"])
+    })
+    
+    if(possibleTweetIdsArray.includes(tweetId)){
+        const getTweetLikesQuery = `SELECT user.username AS likes FROM
+        (tweet LEFT JOIN like ON tweet.tweet_id = like.tweet_id) AS tweetLikes
+        INNER JOIN user ON tweetLikes.like_id = user.user_id
+        WHERE tweet.tweet_id = ${tweetId}`
+        let arrayOfLikes = await databaseConnectionObject.all(getTweetLikesQuery)
+       arrayOfLikes = arrayOfLikes.map((each)=>{
+            return each["likes"]
+        })
+        response.send({"likes":arrayOfLikes})
+    }else{
+        response.status(401)
+        response.send('Invalid request');
+
+    }
+})
+
+
+//getTweetRepliesAPI | API-8 | GET
+
+expressAppInstance.get("/tweets/:tweetId/replies", authenticateUser, async(request, response)=>{
+    let {tweetId} = request.params;
+    tweetId = parseInt(tweetId)
+    const{username} = request;
+
+    let followingUsersWithTheirTweetIdsQuery = `SELECT follower.following_user_id, tweet.tweet_id FROM
+    follower INNER JOIN tweet ON follower.following_user_id = tweet.user_id WHERE follower.follower_user_id = (SELECT user_id FROM user WHERE username LIKE "${username}");`
+
+    let arrayOfFollowingUsersWithTweetIds = await databaseConnectionObject.all(followingUsersWithTheirTweetIdsQuery)
+    console.log(arrayOfFollowingUsersWithTweetIds)
+    let possibleTweetIdsArray=[]
+    arrayOfFollowingUsersWithTweetIds.forEach((each)=>{
+        possibleTweetIdsArray.push(each["tweet_id"])
+    })
+    
+    if(possibleTweetIdsArray.includes(tweetId)){
+        const getTweetRepliesQuery = `SELECT user.name AS name, reply.reply AS reply FROM
+        (reply INNER JOIN user ON reply.user_id = user.user_id) AS replyUser
+        WHERE reply.tweet_id = ${tweetId};`;
+        let arrayOfReplies = await databaseConnectionObject.all(getTweetRepliesQuery)
+        response.send(arrayOfReplies)
+    }else{
+        response.status(401)
+        response.send('Invalid request');
+
+    }
+})
+
+module.exports = expressAppInstance;
